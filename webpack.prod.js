24cc56c389b39d51784dc344e485ab2ec5af2e39
helen0215 <seocc0604@gmail.com>
2022-04-24 18:03:38 +0900

    output publicPath 변경

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
         template: './public/index.html',
-        publicPath: '/dist',
+        publicPath: 'interactive-web/dist',
     }),
     new FileManagerPlugin({
       events: {
