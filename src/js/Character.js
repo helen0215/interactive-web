8115a4b74daa983d5a907231ecf652ba2503ba8d
helen0215 <seocc0604@gmail.com>
2022-05-10 21:14:35 +0900

    캐릭터 엘리먼트 생성 스크립트 추가

---
+++ src/js/Character.js
@@ -0,0 +1,32 @@
+
+export function createCaractor() {
+  const mainElem = document.createElement('div');
+  mainElem.classList.add('character');
+  mainElem.innerHTML = 
+    `<div class="character-face-con character-head">
+        <div class="character-face character-head-face face-front"></div>
+        <div class="character-face character-head-face face-back"></div>
+    </div>
+    <div class="character-face-con character-torso">
+        <div class="character-face character-torso-face face-front"></div>
+        <div class="character-face character-torso-face face-back"></div>
+    </div>
+    <div class="character-face-con character-arm character-arm-right">
+        <div class="character-face character-arm-face face-front"></div>
+        <div class="character-face character-arm-face face-back"></div>
+    </div>
+    <div class="character-face-con character-arm character-arm-left">
+        <div class="character-face character-arm-face face-front"></div>
+        <div class="character-face character-arm-face face-back"></div>
+    </div>
+    <div class="character-face-con character-leg character-leg-right">
+        <div class="character-face character-leg-face face-front"></div>
+        <div class="character-face character-leg-face face-back"></div>
+    </div>
+    <div class="character-face-con character-leg character-leg-left">
+        <div class="character-face character-leg-face face-front"></div>
+        <div class="character-face character-leg-face face-back"></div>
+    </div>`;
+
+  document.querySelector('.stage').appendChild(mainElem);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { createCaractor } from './Character';
+
 import '@/css/reset.css';
 import '@/css/style.css';
 
@@ -6,12 +8,14 @@ const barEl = document.querySelector('.progress-bar');
 const stageEl = document.querySelector('.stage');
 const mousePos = {x: 0, y: 0};
 
-// 스크롤 할 수 있는 영역 문서(body) 높이에서 스크롤바 - 보이는 화면
-let maxScrollValue = getMaxScrollValue();
-
 function getMaxScrollValue() {
   return document.body.offsetHeight - window.innerHeight;
 }
+
+createCaractor();
+
+// 스크롤 할 수 있는 영역 문서(body) 높이에서 스크롤바 - 보이는 화면
+let maxScrollValue = getMaxScrollValue();
 
 window.addEventListener("resize", function () {
   maxScrollValue = getMaxScrollValue();
@@ -45,3 +49,4 @@ if (useragent.indexOf('chrome') < 0 && useragent.indexOf('safari') < 0) {
     document.body.style.cssText = 'height: auto';
     document.body.innerHTML = '<p style="padding: 20px; line-height: 1.6">본 예제는 Full CSS 3D가 사용된 페이지로, webkit 기반 브라우저(크롬, 사파리 등)에서 확인하며 학습하시는게 좋습니다.<br>CSS 3D 부분 외에는 브라우저 상관없이 인터랙티브 웹 페이지 개발을 위해 필수적이고 유용한 내용들을 다루고 있으니 꼭 처음부터 끝까지 공부해 보세요! :)</p>';
 }
+
